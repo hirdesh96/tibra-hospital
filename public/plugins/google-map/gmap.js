e273933ace42f81c987bb7cd60dbb8640b161611
Lokesh K Agrawal <lokesh95@yahoo.com>
2023-07-23 18:10:02 +0530

    changes done with google and tpa

--- public/plugins/google-map/gmap.js
+++ public/plugins/google-map/gmap.js
@@ -3,7 +3,7 @@ window.marker = null;
 function initialize() {
   var map;
 
-  var nottingham = new google.maps.LatLng(51.507351, -0.127758);
+  var nottingham = new google.maps.LatLng(27.603357359875034, 75.14714536693084);
 
   var style = [{
     "featureType": "road.highway",
